5b7628399ebf8764392ee0fe9233fea624ae8d20
xara777 <rafikov.ds777@gmail.com>
2021-03-29 10:18:14 +0500

    Теперь локаторы не содержат сгенерированных частей

--- SDET-Python-homework-1/basic_locators.py
+++ SDET-Python-homework-1/basic_locators.py
@@ -1,9 +1,9 @@
 from selenium.webdriver.common.by import By
 
-LOGIN_LOCATOR = (By.CLASS_NAME, 'responseHead-module-button-1BMAy4')
+LOGIN_LOCATOR = (By.XPATH, '//div[contains(text(), "Войти")]')
 LOGIN_INPUT_LOCATOR = (By.NAME, 'email')
 PASSWORD_INPUT_LOCATOR = (By.NAME, 'password')
-ACCOUNT_BUTTON_LOCATOR = (By.CLASS_NAME, 'right-module-rightWrap-3lL6mf')
+ACCOUNT_BUTTON_LOCATOR = (By.XPATH, '//div[contains(text(), "Баланс")]')
 EXIT_BUTTON_LOCATOR = (By.XPATH, '//a[contains(text(), "Выйти")]')
 PROFILE_LOCATOR = (By.XPATH, '//a[contains(@href,"profile")]')
 FIO_LOCATOR = (By.XPATH, '//div[@data-name="fio"]//input')
