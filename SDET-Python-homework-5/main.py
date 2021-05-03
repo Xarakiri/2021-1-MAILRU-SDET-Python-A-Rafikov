e5f90513345eaa99535ce0cea40b915baf82e56b
xara777 <rafikov.ds777@gmail.com>
2021-05-03 12:42:34 +0500

    Написал метод, который находит общее количество запросов по типу

--- SDET-Python-homework-5/main.py
+++ SDET-Python-homework-5/main.py
@@ -1,4 +1,6 @@
 import argparse
+import re
+from collections import Counter
 from typing import Optional
 from typing import Sequence
 
@@ -10,15 +12,27 @@ def count_lines(filename=PATH_TO_ACCESS_LOG):
         return sum(1 for line in f if line.strip())
 
 
+def count_by_methods(filename=PATH_TO_ACCESS_LOG):
+    pattern = re.compile(r'(GET|POST|PUT|DELETE|UPDATE|PATCH|HEAD|CONNECT|OPTIONS|TRACE)')
+    with open(filename) as f:
+        for line in f:
+            s = re.search(pattern, line)
+            if s:
+                yield s.group()
+
+
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-c', '--count', action='store_true', help='Общее количество запросов')
+    parser.add_argument('-ct', '--count_type', action='store_true', help='Общее количество запросов по типу')
 
     args = vars(parser.parse_args(argv))
 
     if args['count']:
         print(f'Общее количество запросов - {count_lines()}')
+    if args['count_type']:
+        print(f'Общее количество запросов по типу - {Counter(count_by_methods())}')
 
     print(args)
     return 0
