ac479d590f607dbd0a47f140624105ef99f800b8
xara777 <rafikov.ds777@gmail.com>
2021-05-03 14:16:01 +0500

    Написал метод, который ищет топ 5 самых больших запросов, которые завершились 4ХХ ошибкой

--- SDET-Python-homework-5/main.py
+++ SDET-Python-homework-5/main.py
@@ -33,12 +33,30 @@ def top10(filename=PATH_TO_ACCESS_LOG):
                 yield s.group()
 
 
+def top5_by_bytes(filename=PATH_TO_ACCESS_LOG):
+    pattern = re.compile(r'([(\d\.)]+) - - \[(.*?)] "(.*?)" (4\d\d) (\d+)')
+    answer = []
+    with open(filename) as f:
+        for line in f:
+            g = re.match(pattern, line)
+            if g:
+                g = g.groups()
+                url = g[2].split(' ')[1]
+                status = g[3]
+                weight = g[-1]
+                ip = g[0]
+                answer.append((url, status, weight, ip))
+    return list(sorted(answer, key=lambda x: int(x[2]), reverse=True))[:5]
+
+
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-c', '--count', action='store_true', help='Общее количество запросов')
     parser.add_argument('-ct', '--count_type', action='store_true', help='Общее количество запросов по типу')
     parser.add_argument('-mc', '--most_common', action='store_true', help='Топ 10 самых частых запросов')
+    parser.add_argument('-mw', '--most_weight', action='store_true', help='Топ 5 самых больших по размеру запросов,'
+                                                                          'которые завершились клиентской ошибкой')
 
     args = vars(parser.parse_args(argv))
 
@@ -48,6 +66,8 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         print(f'Общее количество запросов по типу - {Counter(count_by_methods())}')
     if args['most_common']:
         print(f'{Counter(top10()).most_common(10)}')
+    if args['most_weight']:
+        print(f'Топ 5 самых больших запросов - {top5_by_bytes()}')
 
     print(args)
     return 0
