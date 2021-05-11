84fd2846bbb5d010752d9c48c96a7b4d99bd08bc
xara777 <rafikov.ds777@gmail.com>
2021-05-11 22:19:25 +0500

    Написал фикстуру для создания и удаления БД

---
+++ SDET-Python-homework-6/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, drop_database
+from sqlalchemy_utils.functions.database import database_exists
+
+from mysql.models import Base
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost:3306/TEST_SQL?unix_socket=/var/run/mysqld/mysqld.sock"
+
+
+@pytest.fixture(scope='session')
+def mysql():
+    if database_exists(SQLALCHEMY_DATABASE_URI):
+        drop_database(SQLALCHEMY_DATABASE_URI)
+    create_database(SQLALCHEMY_DATABASE_URI)
+
+    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    Base.metadata.create_all(bind=engine)
+    yield engine
+    drop_database(SQLALCHEMY_DATABASE_URI)
+
+
+@pytest.fixture(scope='session')
+def client(mysql):
+    session = sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        expire_on_commit=False,
+        bind=mysql
+    )()
+    yield session
+    session.close()
